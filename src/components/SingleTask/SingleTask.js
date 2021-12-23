540d4d0fcd4a8bd933a41c9a54dc0d031add5a0f
Tarikul Islam <tarikul47@gmail.com>
2021-12-24 00:17:48 +0600

    update

--- src/components/SingleTask/SingleTask.js
+++ src/components/SingleTask/SingleTask.js
@@ -4,11 +4,12 @@ import Button from '../Button/Button'
 
 
 const SingleTask = (props) => {
+  console.log(props.task)
   return (
     <>
     <div className="tasks">
       <p>{props.task.name}</p>
-        {/* <input className="taskInput" type="text" value={task.name} onChange={inputData}/> */}
+      {/* <input className="taskInput" type="text" value={task.name} onChange={inputData}/> */}
       <div className="task-update-delete">
         <Accordion id={props.task.id} classAttr={'update view'} icon={'fas fa-eye'}></Accordion>
         <Button classAttr ={'update'} icon={'fas fa-pen-square'}></Button>
@@ -17,9 +18,27 @@ const SingleTask = (props) => {
     </div>
     <div id={`flush-collapse${props.task.id}`} className="accordion-collapse collapse">
       <div style={{ backgroundColor: "#222" }} className="accordion-body tasks">
-        {props.task.name} <br />
-        {props.task.status} <br />
-        {props.task.name}
+      <ol className="list-group list-group-numbered" style={{width: '100%'}}>
+        <li className="list-group-item d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Task Name:</div>
+          </div>
+          <span className="badge bg-primary rounded-pill">{props.task.name}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Task Status:</div>
+          </div>
+          <span className="badge bg-primary rounded-pill">{props.task.status}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Task Created</div>
+          </div>
+          <span className="badge bg-primary rounded-pill">{props.task.createdAt.getDate()}/{props.task.createdAt.getMonth()}/{props.task.createdAt.getFullYear()}</span>
+        </li>
+      </ol>
+    
         <br />
       </div>
     </div>
